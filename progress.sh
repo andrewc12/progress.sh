7c25360bc965b5a29e19a675fe6e0af5288be091
Andrew Innes <andrew.c12@gmail.com>
2015-08-06 13:11:52 +0800

    added boiler plate

--- progress.sh
+++ progress.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# ---------------------------------------------------------------------------
+# progress - Description
+
+# Copyright 2015, Andrew Innes <andrew.c12@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License at <http://www.gnu.org/licenses/> for
+# more details.
+
+# Usage: progress -c command [-t target] [-s sleeptime]
+
+# ---------------------------------------------------------------------------
+
 #CMD="tail -n1 .progress | cut -d' ' -f1"
 #Updates a display version of the variables
 #The original script just reduced the accuracy of the actual variables
